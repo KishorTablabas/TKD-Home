e256d440b75ffe2282a726520b56c097710a6096
Kishor <kishor.jagtap@tablabs.in>
2023-09-04 11:09:42 +0530

    layout modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: "home", pathMatch: "full"
+  },
+  {
+    path:'',
+    component:HomeComponent,
+    loadChildren:()=>import('./Layout/admin-layout/admin-layout.module')
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
